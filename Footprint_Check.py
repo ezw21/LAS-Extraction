0f4770e5723df28900ec3bf741729ae66efae9fa
Edward Wong <edward_wong@eagle.co.nz>
2022-03-10 10:48:00 +1300

    ?Implement ?Chore Initial Commit. Staged Building footprint extraction with 3D building that keep details. ComputerVision algorithm to perform QC, TruePositive >= 0.91.

---
+++ Footprint_Check.py
@@ -0,0 +1,114 @@
+from skimage.metrics import structural_similarity
+import cv2
+import numpy as np
+# np.set_printoptions(threshold=np.inf)
+
+before = cv2.imread("Footprints/3D Basemap_2D_EDW.jpg")
+after = cv2.imread("Footprints/3D Basemap_2D_RAST.jpg")
+
+""" 
+image_size will store image.shape 
+which is a 3obj tuple (dimension_y, dimension_x, RBG)
+"""
+print(before)
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+print(after)
+before_size = before.shape
+after_size = after.shape
+# To see te dimension of before_size
+print("Before_size = " + str(before_size))
+print("After_size = " + str(after_size))  # To see te dimension of after_size
+
+# create after with grids
+after_with_grid = after.copy()
+height, width, channels = after_with_grid.shape
+
+for i in range(0, width, 30):
+    cv2.line(after_with_grid, (i, 0), (i, height), (0, 0, 0), 1)
+for i in range(0, height, 30):
+    cv2.line(after_with_grid, (0, i), (width, i), (0, 0, 0), 1)
+
+# Convert images to grayscale
+before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+
+# Compute SSIM between two images
+(score, diff) = structural_similarity(before_gray, after_gray, full=True)
+print("Image similarity = ", score)
+
+# The diff image contains the actual image differences between the two images
+# and is represented as a floating point data type in the range [0,1]
+# so we must convert the array to 8-bit unsigned integers in the range
+# [0,255] before we can use it with OpenCV
+diff = (diff * 255).astype("uint8")
+
+# Threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(
+    diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+contours = cv2.findContours(
+    thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+
+mask1 = np.zeros(before.shape, dtype="uint8")
+filled_after = after.copy()
+
+bouding_boxes = []
+index = 1
+
+for c in contours:
+    area = cv2.contourArea(c)
+    if area > 300:
+
+        x, y, w, h = cv2.boundingRect(c)
+        bouding_boxes.append((x, y, w, h))
+        cv2.rectangle(before, (x, y), (x + w, y + h), (36, 255, 12), 1)
+        cv2.putText(
+            before,
+            "point" + str(index),
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 0, 0),
+            2,
+        )  # labels
+        cv2.rectangle(after, (x, y), (x + w, y + h), (36, 255, 12), 1)
+        cv2.putText(
+            after,
+            "point" + str(index),
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (36, 255, 12),
+            1,
+        )  # labels
+        cv2.drawContours(mask1, [c], 0, (0, 255, 0), -1)
+        cv2.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
+        index += 1
+
+
+print("box_count = " + str(len(bouding_boxes)) + " \n >> " + str(bouding_boxes))
+cv2.imshow("before", before)
+cv2.imshow("after", after)
+cv2.imshow("diff", diff)
+cv2.imshow("mask1", mask1)
+cv2.imshow("filled after", filled_after)
+cv2.imshow("after with grid", after_with_grid)
+cv2.waitKey(0)
+
+"""
+ Impact assessment part / pseudo ignore
+list_1 = [(x,y,w,h), (obj2.1)]
+list_2 = [(name, shape_size, bla), (obj_2.2)]
+
+>>list_3 = [((x,y,w,h), (name, shape_size, bla)), (obj2.12)]
+
+for i in range(len(list_1)):
+    list_3.append((list_1[i], list2[i]))
+
+print(list_3)
+
+list_1 = bouding_boxes
+
+list_2 = []
+"""
---
+++ Footprints_Extraction.py
@@ -0,0 +1,156 @@
+# Extract building footprint from LAS. Update Line[150:] to run
+# Author:      Edward Wong
+# Created:     23/02/202
+import arcpy
+import time
+import math
+import datetime
+
+
+def LAS_Extraction(myWorkspace, filePath, zone, noise):  # Model
+    start = time.time()
+    print(datetime.datetime.now())
+    print("Extracting 3D building and footprint..\n")
+
+    # To allow overwriting outputs change overwriteOutput option to True.
+    arcpy.env.overwriteOutput = True
+
+    # Check out any necessary licenses.
+    arcpy.CheckOutExtension("3D")
+
+    _zone_las = f"{zone}.las"
+    Dem_tin_tif = arcpy.Raster(f"{zone}.tin.tif")
+    workspace = f"{myWorkspace}\{zone}_"
+    desc = arcpy.Describe(f"{filePath}")
+
+    if desc.usesRelativePath:
+        pathType = 'Relative'
+    else:
+        pathType = 'Absolute'
+
+    # Determine state of statistics
+    if desc.needsUpdateStatistics:
+        if desc.hasStatistics:
+            statistics = 'Out-of-date'
+        else:
+            statistics = 'Missing'
+    else:
+        statistics = 'Current'
+
+    print("LAS Dataset Name: \t\t{0}.las\n"
+          "Point Count: \t\t\t{1}\n"
+          "Surface Constraint Count: \t{2}\n"
+          "Path Type: \t\t\t{3}\n"
+          "Statistics Status: \t\t{4}"
+          .format(desc.basename, desc.pointCount,
+                  desc.constraintCount, pathType,
+                  statistics))
+    print("\nSTART")
+
+    if (noise):
+        print(datetime.datetime.now())
+        _zone_Noise = f"{workspace}Noise"
+        arcpy.ddd.ClassifyLasNoise(_zone_las, method='ISOLATION', edit_las='CLASSIFY',
+                                   withheld='NO_WITHHELD', ground=Dem_tin_tif,
+                                   compute_stats='COMPUTE_STATS',
+                                   low_z='2 Meters', high_z='',
+                                   max_neighbors=10, step_width='8 Meters',
+                                   step_height='8 Meters', out_feature_class=_zone_Noise,
+                                   update_pyramid='UPDATE_PYRAMID')
+        print("Classified LAS Noise")
+
+        # Classify overlap points
+        arcpy.ddd.ClassifyLasOverlap(
+            _zone_las, "0.624 Meters", "DEFAULT", "PROCESS_EXTENT", "COMPUTE_STATS", "UPDATE_PYRAMID")
+        print("Classified overlap points")
+
+#     arcpy.ddd.ChangeLasClassCodes(_zone_las, "0 2 SET SET SET SET;1 2 SET SET SET SET;3 2 SET SET SET SET;4 2 SET SET SET SET;5 2 SET SET SET SET;7 2 SET SET SET SET;18 2 SET SET SET SET;",
+#                                   "COMPUTE_STATS", "DEFAULT", None,
+#                                   "PROCESS_EXTENT", "UPDATE_PYRAMID")
+#     print("Changed LAS ClassCode")
+
+    print(datetime.datetime.now())
+    # Filter LAS to Class_6
+    arcpy.management.MakeLasDatasetLayer(
+        _zone_las, _zone_las, class_code=[6])
+
+    # Process: LAS Point Statistics As Raster (LAS Point Statistics As Raster) (management)
+    _zone_Raster = f"{workspace}Raster"
+    arcpy.management.LasPointStatsAsRaster(in_las_dataset=_zone_las, out_raster=_zone_Raster,
+                                           method="PREDOMINANT_CLASS", sampling_type="CELLSIZE", sampling_value=0.5)
+    _zone_Raster = arcpy.Raster(_zone_Raster)
+
+    # Process: Raster to Polygon (Raster to Polygon) (conversion)
+    _zone_Polygon = f"{workspace}BuildingRaw"
+    with arcpy.EnvManager(outputMFlag="Disabled", outputZFlag="Disabled"):
+        arcpy.conversion.RasterToPolygon(in_raster=_zone_Raster, out_polygon_features=_zone_Polygon, simplify="NO_SIMPLIFY",
+                                         raster_field="Value", create_multipart_features="SINGLE_OUTER_PART", max_vertices_per_feature=None)
+    _zone_PolygonSmooth = f"{workspace}BuildingSmooth"
+    arcpy.cartography.SmoothPolygon(_zone_Polygon, _zone_PolygonSmooth,
+                                    "PAEK", "5 Meters", "FIXED_ENDPOINT", "RESOLVE_ERRORS", None)
+
+    # Low Dense
+    _zone_LowDensePolygon, Count_one = arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=_zone_PolygonSmooth, selection_type="NEW_SELECTION", where_clause="Shape_Length > (Shape_Area * 1.05) And Shape_Area > 15", invert_where_clause="")
+    print(Count_one)
+
+    _zone_LowDensePolygonAggr = f"{workspace}BuildingJoin"
+    arcpy.cartography.AggregatePolygons(_zone_LowDensePolygon, _zone_LowDensePolygonAggr,
+                                        "0.51 Meters", "10 SquareMeters", "10 SquareMeters", "ORTHOGONAL")
+
+    _zone_DensePolygon = f"{workspace}DensePolygon"
+    arcpy.cartography.SimplifyBuilding(_zone_LowDensePolygonAggr, _zone_DensePolygon,
+                                       "1 Meters", "0 SquareMeters", "NO_CHECK", None, "NO_KEEP")
+
+    # Process: Eliminate Polygon Part (Eliminate Polygon Part) (management)
+    _zone_DensePolygonClean = f"{workspace}DenseBuildingClean"
+    arcpy.management.EliminatePolygonPart(in_features=_zone_DensePolygon, out_feature_class=_zone_DensePolygonClean,
+                                          condition="AREA", part_area="10 SquareMeters", part_area_percent=0, part_option="CONTAINED_ONLY")
+
+    # High Dense
+
+    # Process: Select Layer By Attribute (Select Layer By Attribute) (management)
+    _zone_HighDensePolygon, Count_two = arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=_zone_PolygonSmooth, selection_type="NEW_SELECTION", where_clause="Shape_Length < (Shape_Area * 1.05) And Shape_Area > 15", invert_where_clause="")
+    print(Count_two)
+
+    _zone_HighDensePolygonSimp = f"{workspace}HighDensePolygon"
+    arcpy.cartography.SimplifyBuilding(_zone_HighDensePolygon, _zone_HighDensePolygonSimp,
+                                       "1 Meters", "0 SquareMeters", "NO_CHECK", None, "NO_KEEP")
+
+    # Process: Eliminate Polygon Part (Eliminate Polygon Part) (management)
+    _zone_HighDensePolygonClean = f"{workspace}HighDenseBuildingClean"
+    arcpy.management.EliminatePolygonPart(in_features=_zone_HighDensePolygonSimp, out_feature_class=_zone_HighDensePolygonClean,
+                                          condition="AREA", part_area="10 SquareMeters", part_area_percent=0, part_option="CONTAINED_ONLY")
+
+    _zone_PolygonClean = f"{workspace}BuildingClean"
+    arcpy.Merge_management([_zone_DensePolygonClean, _zone_HighDensePolygonClean],
+                           _zone_PolygonClean, "", "ADD_SOURCE_INFO")
+
+    # Process: Regularize Building Footprint (Regularize Building Footprint) (3d)
+    _zone_Footprints = f"{workspace}Footprints"
+    arcpy.ddd.RegularizeBuildingFootprint(in_features=_zone_PolygonClean, out_feature_class=_zone_Footprints, method="RIGHT_ANGLES", tolerance=1, densification=1,
+                                          precision=0.15, diagonal_penalty=1.5, min_radius=0.1, max_radius=1000000, alignment_feature="", alignment_tolerance="", tolerance_type="DISTANCE")
+
+    print(datetime.datetime.now())
+    # Process: LAS Building Multipatch (LAS Building Multipatch) (3d)
+    _zone_3D = (workspace + "3D")
+    arcpy.ddd.LasBuildingMultipatch(in_las_dataset=_zone_las, in_features=_zone_Footprints, ground=Dem_tin_tif,
+                                    out_feature_class=_zone_3D, point_selection="BUILDING_CLASSIFIED_POINTS", simplification="0.1 Meters")
+
+# Display total time consumption
+    minute = 0
+    seconds = (time.time() - start)
+    while (seconds >= 60):
+        seconds -= 60
+        minute += 1
+    print("Total time consumption = {}(m) {}(s)".format(
+        minute, math.ceil(seconds)))
+
+
+if __name__ == '__main__':
+    # Global Environment settings
+    myWorkspace = r"C:\Users\exw\Documents\ArcGIS\Projects\PROJECT\PROJECT.gdb"
+    myFilePath = r"C:\PHYSICAL_PATH\LAS_File.las"
+    with arcpy.EnvManager(scratchWorkspace=myWorkspace, workspace=myWorkspace):
+        LAS_Extraction(myWorkspace, myFilePath, "LAS_FILE", False)
